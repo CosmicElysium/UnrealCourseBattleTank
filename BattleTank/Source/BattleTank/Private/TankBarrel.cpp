b2e64744f21eb798e655d5c28579b0a786f63651
SIVIGAMING-PC\Sivi <alexandersivitilli@gmail.com>
2018-06-26 14:44:33 +0200

    Barrel changes elevation to point in direction of crosshair (with clamped min/max elevations).

--- BattleTank/Source/BattleTank/Private/TankBarrel.cpp
+++ BattleTank/Source/BattleTank/Private/TankBarrel.cpp
@@ -1,11 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "TankBarrel.h"
-
+#include "Engine/World.h"
 
 
 
 void UTankBarrel::Elevate(float RelativeSpeed)
 {
-	return;
+	auto ClampedRelativeSpeed = FMath::Clamp<float>(RelativeSpeed, -1, 1);
+	auto ElevationChange = ClampedRelativeSpeed * MaxDegreesPerSecond * GetWorld()->DeltaTimeSeconds;
+	auto RawNewElevation = RelativeRotation.Pitch + ElevationChange;
+	auto ClampedNewElevation = FMath::Clamp<float>(RawNewElevation, MinElevationDegrees, MaxElevationDegrees);
+	SetRelativeRotation(FRotator(ClampedNewElevation, 0, 0));
 }
