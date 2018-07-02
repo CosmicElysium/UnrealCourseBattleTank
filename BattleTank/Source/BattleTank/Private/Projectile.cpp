9c61f46c4dde57cd17abb09d34e04c9077e12539
SIVIGAMING-PC\Sivi <alexandersivitilli@gmail.com>
2018-07-02 22:45:16 +0200

    Added force at tank tracks when throttles are pressed

--- BattleTank/Source/BattleTank/Private/Projectile.cpp
+++ BattleTank/Source/BattleTank/Private/Projectile.cpp
@@ -33,8 +33,7 @@ void AProjectile::Tick(float DeltaTime)
 void AProjectile::LaunchProjectile(float Speed)
 {
 	
-	auto Time = GetWorld()->GetTimeSeconds();
-	UE_LOG(LogTemp, Warning, TEXT("%f: Projectile fired at %f m/s!"), Time, Speed);
+
 	ProjectileMovement->SetVelocityInLocalSpace(FVector::ForwardVector * Speed);
 	ProjectileMovement->Activate();
 }
